a246761e272a6a056e5788662063feeef53147b3
xerocross <adam.cross.inc@gmail.com>
2016-04-15 18:40:22 -0500

    Add basic elements of a LinkedList object

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,20 @@
 package hashTable;
 
-public class LinkedList {
 
+class ListItem<T> {
+	public T element;
+	public T next;
+	
+	ListItem (T element, T next){
+		this.element = element;
+		this.next = next;
+	}
+}
+
+public class LinkedList<T> {
+	public ListItem<T> firstItem;
+
+	LinkedList(ListItem<T> firstItem){
+		this.firstItem = firstItem;
+	}
 }
